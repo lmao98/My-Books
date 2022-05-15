f3cc4fb09fb3d4858a09aa21e6d28196217c20f0
Abdullah Albayati <dark.king1154@gmail.com>
2022-05-15 13:19:14 -0500

    Created Oauth login page

--- client/books/src/main.js
+++ client/books/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+import gAuthPlugin from 'vue3-google-oauth2'
+let gauthClientId = '206051238634-f1i031k7v3t8acpvcsiq4s14rsisqba2.apps.googleusercontent.com';
+app.use(gAuthPlugin, {
+  clientId: gauthClientId,
+  scope: 'email',
+  prompt: 'consent',
+})
+
+app.mount('#app')
