9e23498f52b133ca5ea5d0eddbfb3d4b09a51708
Abdullah Albayati <dark.king1154@gmail.com>
2022-05-16 07:51:23 -0500

    trying firebase for google Oauth

---
+++ client/src/API/firebase.js
@@ -0,0 +1,17 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAPA-hsjphdLj3kkSeZM6f_G6AwqAG5u7s",
+//   authDomain: "mybooks-45b8e.firebaseapp.com",
+//   projectId: "mybooks-45b8e",
+//   storageBucket: "mybooks-45b8e.appspot.com",
+//   messagingSenderId: "759705676353",
+//   appId: "1:759705676353:web:f86f69da91756f2535b4c0",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
