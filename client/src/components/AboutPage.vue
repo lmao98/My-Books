06d612f067aa121bb2dbe54d964baaf537749531
Abdullah Albayati <dark.king1154@gmail.com>
2022-05-16 09:52:02 -0500

    added logic to about page

--- client/src/components/AboutPage.vue
+++ client/src/components/AboutPage.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>About</div>
+  <div>
+    <a href="http://https://www.linkedin.com/in/abdullah-albayati-85b3771b4/">Linkedin</a>
+    <p>
+      I am a full stack software engineer. In my previous math classes at college I kept hearing students say those concepts are not used in
+      real life jobs. And I totally disagree with them, solving complex logarithmic problems with code makes me very excited. In the next
+      phase of my career, I not only want to use my problem solving skills to make the application satisfy what the customer needs, but I
+      also want to work on the technically challenging facets of the application too. In my previous projects, I have led my teams to
+      success with clear instructions and great communications. I am very passionate about software development and a quick learner since I
+      was able to learn so many hard concepts in a very short amount of time.
+    </p>
+  </div>
 </template>
 
 <script>
