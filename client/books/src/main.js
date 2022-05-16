053d372bf868c14ab54c063a5722fff37bf5dafc
Abdullah Albayati <dark.king1154@gmail.com>
2022-05-15 23:18:14 -0500

    changes to book

--- client/books/src/main.js
+++ client/books/src/main.js
@@ -1,14 +1,22 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+// import { createApp } from "vue";
+// import App from "./App.vue";
 
-const app = createApp(App);
-import gAuthPlugin from "vue3-google-oauth2";
-let gauthClientId =
-  "206051238634-f1i031k7v3t8acpvcsiq4s14rsisqba2.apps.googleusercontent.com";
-app.use(gAuthPlugin, {
-  clientId: gauthClientId,
-  scope: "email",
-  prompt: "consent",
-});
+// const app = createApp(App);
+// import gAuthPlugin from "vue3-google-oauth2";
+// let gauthClientId =
+//   "206051238634-f1i031k7v3t8acpvcsiq4s14rsisqba2.apps.googleusercontent.com";
+// app.use(gAuthPlugin, {
+//   clientId: gauthClientId,
+//   scope: "email",
+//   prompt: "consent",
+// });
 
-app.mount("#app");
+// app.mount("#app");
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
